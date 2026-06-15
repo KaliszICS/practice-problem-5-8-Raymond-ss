7e03ca06bdfcb3c61192defc1fe69d652645577d
Raymond-ss <343383188@tdsb.ca>
2026-06-15 18:54:10 +0000

    Finish 5.8

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,30 @@
+import java.util.HashMap;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+
+	public static HashMap<String, Integer> combineParallelArrays(String[] names, int[] ages) {
+		HashMap <String, Integer> map = new HashMap<>();
+		for (int i =0; i < names.length; i++){
+			map.put(names[i], ages[i]);
+		}
+	return map;
+	}
+	
+	public static void increaseAge(HashMap<String, Integer> map, String name) {
+		if (map.containsKey(name)) {
+			map.put(name, map.get(name) + 1);
+		}
+	}
+
+	public static void replaceName (HashMap<String, Integer> map, String oldName, String newName) {
+		if (map.containsKey(oldName)) {
+			Integer age = map.remove(oldName);
+			map.put(newName, age);
+		}
 	}
 
 }
